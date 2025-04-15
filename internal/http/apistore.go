6eb9d228bfd3b4b95e631e5b70fd43a95f796461
Aaron Mathis <aaron.mathis@gmail.com>
2025-04-14 23:48:14 -0500

    Update apistore.go

--- internal/http/apistore.go
+++ internal/http/apistore.go
@@ -23,12 +23,12 @@ func (a *APIMetricStore) Close() error {
 }
 
 func (a *APIMetricStore) QueryInstant(metric string, filters map[string]string) ([]model.MetricRow, error) {
-	utils.Debug("🔍 QueryInstant: %s", metric)
+	utils.Debug("QueryInstant: %s", metric)
 	return a.Store.QueryInstant(metric, filters)
 }
 
 func (a *APIMetricStore) QueryRange(metric string, start, end time.Time, filters map[string]string) ([]model.Point, error) {
-	utils.Debug("📈 QueryRange: %s (%s - %s)", metric, start.Format(time.RFC3339), end.Format(time.RFC3339))
+	utils.Debug("QueryRange: %s (%s - %s)", metric, start.Format(time.RFC3339), end.Format(time.RFC3339))
 	return a.Store.QueryRange(metric, start, end, filters)
 }
 func (a *APIMetricStore) QueryMultiInstant(metricNames []string, filters map[string]string) ([]model.MetricRow, error) {
